3a9d27a93a51a4fa9124e591ad059c769cdaa475
arathy <arathyrajeesh2@gmail.com>
2026-08-06 10:41:35 +0530

    feat: initialize Django settings and update static file path in Vercel configuration

--- acadeno_outcomes/settings.py
+++ acadeno_outcomes/settings.py
@@ -86,7 +86,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'quiz' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
